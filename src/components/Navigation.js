c0ee371db6cf45f2e806888fca03a24fbb28cd9d
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2024-01-30 10:42:03 +0100

    Add Home Page Header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { Outlet, NavLink } from "react-router-dom";
 const Navigation = (props) => {
     return (
         <>
-        <nav className="sticky bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+        <nav className="absolute bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
             <ul className="grid h-full max-w-lg grid-cols-3 mx-auto">
             {
                 props.nav_list.map((item) => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
+import logo from '../assets/logo.png';
+
+import Location_header from "../components/location_header";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+    <div>
+        <div className="w-full flex justify-center my-12 logo">
+            <img className="w-32" src={logo} alt="image logo"></img>
+        </div>
+
+        <Location_header></Location_header>
+
+    </div>
+    );
 };
   
 export default Home;
